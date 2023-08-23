e81c67aa5dbe09f5bb54e380c96e7ec9d2137cb8
Monicah <98586425+MonahDipsy@users.noreply.github.com>
2023-08-23 16:58:27 +0300

    Add

---
+++ functions.c
@@ -0,0 +1,50 @@
+void f(void)
+{
+    int i = 0;
+    while (1)
+    {
+        ++i;
+        if (i % 10000000 == 0)
+        {
+            // Some action here
+        }
+    }
+}
+
+void g(void)
+{
+    int i = 0;
+    while (1)
+    {
+        ++i;
+        if (i % 10000000 == 0)
+        {
+            // Some action here
+        }
+    }
+}
+
+void h(void)
+{
+    int i = 0;
+    while (1)
+    {
+        ++i;
+        if (i % 10000000 == 0)
+        {
+            // Some action here
+        }
+    }
+}
+
+int main()
+{
+    while (1)
+    {
+        // Infinite loop
+    }
+    return 0;
+}
+
+
+
